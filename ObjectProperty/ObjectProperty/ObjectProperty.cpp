66ec2f448e716f4b183b575e8a4a99c1602ddaca
HideyukiWada <e14m5228@soka-u.jp>
2015-06-20 17:10:40 +0900

    Modify: DBを参照する関数を追加

--- ObjectProperty/ObjectProperty/ObjectProperty.cpp
+++ ObjectProperty/ObjectProperty/ObjectProperty.cpp
@@ -9,6 +9,7 @@
 #include "SIGService.h"
 #include <tchar.h>
 #include <string>
+#include <sstream>
 
 bool start = false;
 
@@ -28,16 +29,30 @@ public:
 	MyService(std::string name) : SIGService(name){};
 	~MyService();
 	void onRecvMsg(sigverse::RecvMsgEvent &evt);
-	void mysqlRead(std::string object);
-	void mysqlWrite(string Property);
 	double onAction();
 	void InitToMysql();
+	void mysqlRead(void);
+	void mysqlSearchInit(void);
+	void mysqlSearch(void);
+	void mysqlWrite(string Property);
 	void strSplit(string msg, string separator);
+	string Int2String(int number);
 private:
+	//typedef std::pair<std::string, int> Property;
+	//std::vector<Property> Properties;
+
+	std::map<std::string, int>Properties;
+	std::vector<std::string> Obj_list;
+	std::vector<std::string> List;
+	std::vector<std::string> RequestedProperties;
+
 	string obj_id;
+	string property;
 	string attribute;
 	string headStr;
 	string bodyStr;
+
+	int obj_count;
 
 };
 
@@ -54,6 +69,9 @@ double MyService::onAction()
 		cin >> Property;
 		if (Property != "no") {
 			this->mysqlWrite(Property);
+			this->sendMsg("AvatarView",Property);
+			std::string msg = "PickUp " + Property;
+			this->sendMsg("man_0", msg);
 			this->sendMsg("ChangeObj","‚ ‚è");
 		}
 		else {
@@ -73,7 +91,7 @@ void MyService::onRecvMsg(sigverse::RecvMsgEvent &evt)
 		if (msg == "Start") {
 			cout << msg << endl;
 			//‰ŠúÝ’è
-			this->InitToMysql();
+			//this->InitToMysql();
 		}
 		else if (headStr == "id") {
 			obj_id = bodyStr;
@@ -90,7 +108,25 @@ void MyService::onRecvMsg(sigverse::RecvMsgEvent &evt)
 		if (msg == "Start") {
 			cout << msg << endl;
 			//‰ŠúÝ’è
-			this->InitToMysql();
+			//this->InitToMysql();
+		}
+	}
+	else if (sender == "Robot"){
+		if (msg == "Start") {
+			cout << msg << endl;
+		}
+		else if (headStr == "Request"){
+			property = bodyStr;
+			RequestedProperties.push_back(property);
+			this->mysqlRead();
+		}
+		else if (headStr == "Search"){
+			if (bodyStr == "Init"){
+				this->mysqlSearchInit();
+			}
+			else{
+				this->mysqlSearch();
+			}
 		}
 	}
 }
@@ -157,7 +193,7 @@ void MyService::mysqlWrite(string Property){
 }
 
 // Read from the database
-void MyService::mysqlRead(string name){
+void MyService::mysqlRead(void){
 	try {
 		Driver *driver;
 		Connection *con;
@@ -170,22 +206,27 @@ void MyService::mysqlRead(string name){
 		con = driver->connect("tcp://150.37.224.108:3306", "sig", "sigverse");
 		/* Connect to the MySQL test database */
 		con->setSchema("ObjectProperties");
-		pstmt = con->prepareStatement("select id from " + obj_id + ";");
+		stmt = con->createStatement();
+		std::cout << "Mysql‚ÉÚ‘±‚µ‚Ü‚µ‚½" << std::endl;
+
+		obj_count = 0;
+		pstmt = con->prepareStatement("select id from " + property + ";");
 		res = pstmt->executeQuery();
-		// count rows
-		//ResultSet *rows;
-		//pstmt = con->prepareStatement("select count(*) from " + objConcept + ";");
-		//rows = pstmt->executeQuery();
-		//rows->next();
-		//int count = rows->getInt(1);
-		//cout << count << endl;
-		cout << "message = " << obj_id << endl;
+		cout << "Property = " << property << endl;
 		cout << "***** targetOBJ *****" << endl;
 		while (res->next()) {
 			string select = res->getString("id");
 			cout << select << endl;
+			Obj_list.push_back(select);
+			obj_count++;
 		}
 		cout << "*********************" << endl;
+
+		string num_obj;
+		string tmp = Int2String(obj_count);
+		num_obj = "Count:" + tmp;
+		std::cout << num_obj << std::endl;
+		sendMsgToCtr("Robot", num_obj);
 
 		delete res;
 		delete pstmt;
@@ -202,6 +243,123 @@ void MyService::mysqlRead(string name){
 	cout << endl;
 }
 
+void MyService::mysqlSearchInit(void){
+	try {
+		Driver *driver;
+		Connection *con;
+		Statement *stmt;
+		ResultSet *res;
+		PreparedStatement *pstmt;
+
+		/* Create a connection */
+		driver = get_driver_instance();
+		con = driver->connect("tcp://150.37.224.108:3306", "sig", "sigverse");
+		/* Connect to the MySQL test database */
+		con->setSchema("ObjectProperties");
+		stmt = con->createStatement();
+		std::cout << "Mysql‚ÉÚ‘±‚µ‚Ü‚µ‚½" << std::endl;
+
+		pstmt = con->prepareStatement("select table_name from information_schema.columns where column_name='id' and table_schema='ObjectProperties';");
+		res = pstmt->executeQuery();
+		while (res->next()) {
+			string select = res->getString("table_name");
+			List.push_back(select);
+		}
+		std::cout << "List size is " << List.size() << std::endl;
+		std::string msg = "Search:init_finish";
+		std::cout << msg << std::endl;
+		sendMsgToCtr("Robot", msg);
+
+		delete res;
+		delete pstmt;
+		delete con;
+		// Error handler
+	}
+	catch (sql::SQLException &e) {
+		cout << "# ERR: SQLException in " << __FILE__;
+		cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
+		cout << "# ERR: " << e.what();
+		cout << " (MySQL error code: " << e.getErrorCode();
+		cout << ", SQLState: " << e.getSQLState() << " )" << endl;
+	}
+	cout << endl;
+}
+
+
+void MyService::mysqlSearch(void){
+	try {
+		Driver *driver;
+		Connection *con;
+		Statement *stmt;
+		PreparedStatement *pstmt;
+		ResultSet *rows;
+
+		/* Create a connection */
+		driver = get_driver_instance();
+		con = driver->connect("tcp://150.37.224.108:3306", "sig", "sigverse");
+		/* Connect to the MySQL test database */
+		con->setSchema("ObjectProperties");
+		stmt = con->createStatement();
+		std::cout << "Mysql‚ÉÚ‘±‚µ‚Ü‚µ‚½" << std::endl;
+
+
+
+		for (int i = 0; i < List.size(); i++){
+			std::vector<std::string>::iterator LItr = std::find(RequestedProperties.begin(), RequestedProperties.end(), List[i]);
+			if (LItr != RequestedProperties.end()){
+				continue;
+			}
+			else{
+				for (int j = 0; j < Obj_list.size(); j++){
+					string query;
+					//‰½‰ñList[i]‚ðŒŸõ‚µ‚½‚©
+					query = "select id from " + List[i] + " where id='" + Obj_list[j] + "';";
+					//std::cout << query << std::endl;
+					pstmt = con->prepareStatement(query);
+					rows = pstmt->executeQuery();
+					rows->next();
+					int count = rows->rowsCount();
+					if (count == 0){
+						cout << "not exist" << endl;
+					}
+					else {
+						std::cout << "’Ç‰Á‚µ‚Ü‚·" << std::endl;
+						//Properties.push_back(Property(List[i], num + 1));
+						std::map<std::string, int>::iterator itr = Properties.find(List[i]);
+						if (itr == Properties.end()){
+							//std::cout << "insert:" << List[i] << std::endl;
+							Properties.insert(std::map<std::string, int>::value_type(List[i], 1));
+						}
+						else{
+							itr->second++;
+						}
+					}
+				}
+			}
+		}
+		std::cout << (unsigned int)Properties.size() << std::endl;
+		std::map<std::string, int>::iterator itr = Properties.begin();
+		while (itr != Properties.end()){
+			std::cout << (*itr).first << ":" << (*itr).second << std::endl;
+			++itr;
+		}
+
+		delete con;
+		delete pstmt;
+		delete rows;
+		// Error handler
+	}
+	catch (sql::SQLException &e) {
+		cout << "# ERR: SQLException in " << __FILE__;
+		cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
+		cout << "# ERR: " << e.what();
+		cout << " (MySQL error code: " << e.getErrorCode();
+		cout << ", SQLState: " << e.getSQLState() << " )" << endl;
+	}
+	cout << endl;
+}
+
+
 void MyService::InitToMysql(){
 	try {
 		Driver *driver;
@@ -217,13 +375,12 @@ void MyService::InitToMysql(){
 		stmt->execute("DROP TABLE IF EXISTS name");
 		stmt->execute("DROP TABLE IF EXISTS color");
 		stmt->execute("DROP TABLE IF EXISTS shape");
-		stmt->execute("DROP TABLE IF EXISTS state");
+		stmt->execute("DROP TABLE IF EXISTS attachment");
 		stmt->execute("DROP TABLE IF EXISTS how_to_use");
-		stmt->execute("DROP TABLE IF EXISTS gabunomi");
 
 		// create concept
 		stmt->execute("CREATE TABLE attribute  (attribute char (50)) ");
-		stmt->execute("INSERT INTO attribute VALUES ('name'),('color'),('shape'),('state'),('how to use')");
+		stmt->execute("INSERT INTO attribute VALUES ('name'),('color'),('shape'),('attachment'),('how_to_use')");
 		// create name
 		stmt->execute("CREATE TABLE name  (property char (50) unique) ");
 		// create color
@@ -231,7 +388,7 @@ void MyService::InitToMysql(){
 		// create shape
 		stmt->execute("CREATE TABLE shape  (property char (50) unique) ");
 		// create state
-		stmt->execute("CREATE TABLE state  (property char (50) unique) ");
+		stmt->execute("CREATE TABLE attachment  (property char (50) unique) ");
 		// create how_to_use
 		stmt->execute("CREATE TABLE how_to_use  (property char (50) unique) ");
 
@@ -261,6 +418,13 @@ void MyService::strSplit(string msg, string separator) {
 	bodyStr = body;
 }
 
+string MyService::Int2String(int number){
+	stringstream ss;
+	ss << number;
+	return ss.str();
+
+}
+ 
 int main(int argc, char** argv)
 {
 	MyService srv("sigverse_DB");
